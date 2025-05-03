345720b5f5f2cd094e5281e741b976106f5482b3
Suppyour <mporsev@mail.ru>
2025-05-03 17:38:11 +0500

    (#2) Add gitignore

--- SecondApp/obj/Debug/net9.0/SecondApp.AssemblyInfo.cs
+++ SecondApp/obj/Debug/net9.0/SecondApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecondApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa7138a84a53c7a996ba6ab3082945e2bdb16e7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecondApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecondApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
